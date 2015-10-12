ee61b9a1a6a3a415d53847b4bff2095e520dbc17
Niall O'Kane <okanenm@gmail.com>
2015-10-12 16:41:37 -0700

    set up api for Account authentication

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+# .. Imports
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
+urlpatterns = patterns(
+     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
+
+    url('^.*$', IndexView.as_view(), name='index'),
+)
