3540b961728b4b2c526d7fb7513c17fd43e03fc9
Niall O'Kane <okanenm@gmail.com>
2015-10-12 13:50:07 -0700

    Call IndexView

--- connect_us/urls.py
+++ connect_us/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+from connect_us.views import IndexView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'connect_us.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
 
-    url(r'^admin/', include(admin.site.urls)),
+    url('^.*$', IndexView.as_view(), name='index'),
 )
