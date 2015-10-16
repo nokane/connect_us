04242554ac48c4b81244575b34fa15cb4008a2a9
Niall O'Kane <okanenm@gmail.com>
2015-10-16 11:25:07 -0700

    Implement profile-settings feature

---
+++ static/javascripts/profiles/services/profiles.service.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  function Profile($http) {
+
+
+    function destroy(username) {
+
+      return $http.delete('/api/v1/accounts/' + username + '/');
+    }
+
+    function get(username) {
+      return $http.get('/api/v1/accounts/' + username + '/');
+    }
+
+    function update(profile) {
+      return $http.put('/api/v1/accounts/' + profile.username + '/', profile);
+    }
+    
+    var Profile = {
+      destroy: destroy,
+      get: get,
+      update: update
+    };
+
+    return Profile;
+
+  }
+  Profile.$inject = ['$http'];
+  angular
+    .module('connectus.profiles.services', [])
+    .factory('Profile', Profile);
+
+})();
